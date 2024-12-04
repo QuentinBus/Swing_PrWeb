7389bacd6c12a8f1e8a7d8cbaedae252b5e66321
QuentinBus <quentin.bussard@eleves.ec-nantes.fr>
2024-12-04 14:56:52 +0100

    creation projet

---
+++ src/main/java/org/centrale/tpweb_spring/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package org.centrale.tpweb_spring;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
