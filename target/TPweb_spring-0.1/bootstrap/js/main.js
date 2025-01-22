e2d60fa0e27860113e74dd3c304c9c27e2058ee5
QuentinBus <quentin.bussard@eleves.ec-nantes.fr>
2025-01-22 15:40:07 +0100

    Add function for DS

--- target/TPweb_spring-0.1/bootstrap/js/main.js
+++ target/TPweb_spring-0.1/bootstrap/js/main.js
@@ -24,3 +24,40 @@ function deleteBook(BookId,ButtonRef){
         });
     }
 }
+
+function returnBorrowSuccess(theResult, buttonRef) {
+  if (buttonRef !== null) {
+    // Get TD that owns the button
+    var refTD = buttonRef.parentElement;
+    if (refTD !== null) {
+      // Remove button
+      refTD.removeChild(buttonRef);
+      // Set return date
+      var currentDate = new Date(((Date)(theResult.returnedValue)));
+      var currentDateStr = currentDate.toLocaleDateString();
+      var text = document.createTextNode(currentDateStr);
+      refTD.appendChild(text);
+    }
+  }
+}
+
+function returnBorrow(buttonRef, borrowId) {
+  if (borrowId > 0) {
+    // Collect data - empty
+
+    // Ajax call
+    $.ajax({
+      url: "returnBorrow.do",
+      method: "POST",
+      data: {
+        "id": borrowId,
+      },
+      success: function (theResult) {
+        returnBorrowSuccess(theResult, buttonRef);
+      },
+      error: function (theResult, theStatus, theError) {
+        console.log("Error : " + theStatus + " - " + theResult);
+      }
+    });
+  }
+}
